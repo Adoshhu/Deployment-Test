c65b25da3479b7e597674331d9de4c3821671371
Adoshhu <adrianumalir@gmail.com>
2023-03-20 15:51:04 +0800

    test

--- db/migrate/20230320071639_add_folder_name_to_folders.rb
+++
@@ -1,5 +0,0 @@
-class AddFolderNameToFolders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :folders, :folderName, :string
-  end
-end
--- db/migrate/20230320074020_add_user_id_to_folders.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToFolders < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :folders, :user, null: false, foreign_key: true
-  end
-end
