e6bdc28799483cfb47448261ea0e56bd5077c043
Adoshhu <adrianumalir@gmail.com>
2023-03-21 09:10:21 +0800

    test

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec bundle install
-bundle exec rails webpacker:install
 bundle exec rails db:migrate
 bundle exec rails db:seed
+bundle exec rails webpacker:install
